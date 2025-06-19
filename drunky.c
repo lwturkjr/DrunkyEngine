0ccdb601887384a245d738dc55a2428b8c0ffae3
Lloyd W Turk Jr. <xerodill@gmail.com>
2025-06-19 02:32:59 -0400

    just a few more videos

--- drunky.c
+++ drunky.c
@@ -4,18 +4,13 @@
 int main() {
 
     AllInit();
-
+    int index = 0;
     U64 playBitBoard = 0ULL;
 
-    printf("Start:\n\n");
+    SETBIT(playBitBoard, 61);
     PrintBitBoard(playBitBoard);
 
-    playBitBoard |= (1ULL << SQ64(D2));
-    printf("D2 Added:\n\n");
-    PrintBitBoard(playBitBoard);
-
-    playBitBoard |= (1ULL << SQ64(G2));
-    printf("G2 Added:\n\n");
+    CLRBIT(playBitBoard, 61);
     PrintBitBoard(playBitBoard);
     
     return 0;
